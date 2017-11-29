3311109c047738e01932cc714c00e1a1effc2f3d
baijing <315346344@qq.com>
2017-11-29 22:34:14 +0800

    主页完成

---
+++ public/admin/js/common.js
@@ -0,0 +1,44 @@
+    //关闭进度环
+    NProgress.configure({ showSpinner: false });
+
+    //发送ajax请求时触发
+    $(document).ajaxStart(function() {
+        NProgress.start();
+    });
+    //发送ajax请求结束时触发
+    $(document).ajaxStop(function() {
+        NProgress.done();
+    });
+
+
+
+    $(".child").prev().on("click", function() {
+        $(this).next().slideToggle();
+    })
+
+
+    $(".icon_menu").on("click", function() {
+        $(".lt_main").toggleClass("now");
+        $(".lt_aside").toggleClass("now");
+    })
+
+    $(".icon_logout").on("click", function() {
+        $("#logoutModal").modal("show");
+
+        $(".btn_logout").off().on("click", function() {
+
+            $.ajax({
+                type: "get",
+                url: "/employee/employeeLogout",
+                success: function(data) {
+                    if (data.success) {
+                        location.href = "login.html";
+                    }
+                }
+            })
+
+
+
+        })
+
+    })
